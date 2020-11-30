fc084e1131aec07d54bb81c7aa1db23fd04eadf1
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-12-01 05:54:22 +0700

    First Post

---
+++ app/src/main/java/com/example/academyjetpack/data/ContentEntity.kt
@@ -0,0 +1,5 @@
+package com.example.academyjetpack.data
+
+data class ContentEntity (
+    var content: String?
+        )
---
+++ app/src/main/java/com/example/academyjetpack/ui/reader/content/ModuleContentFragment.kt
@@ -0,0 +1,27 @@
+package com.example.academyjetpack.ui.reader.content
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.academyjetpack.R
+
+class ModuleContentFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_module_content, container, false)
+    }
+
+    companion object {
+
+    }
+}
