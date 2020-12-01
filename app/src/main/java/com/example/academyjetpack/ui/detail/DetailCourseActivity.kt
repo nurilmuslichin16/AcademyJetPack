67179796a76e4f43be91820de04db64ca9d883f5
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-12-02 05:39:47 +0700

    Layouting fix

--- app/src/main/java/com/example/academyjetpack/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/example/academyjetpack/ui/detail/DetailCourseActivity.kt
@@ -11,13 +11,8 @@ class DetailCourseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_course)
+
         setSupportActionBar(findViewById(R.id.toolbar))
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
